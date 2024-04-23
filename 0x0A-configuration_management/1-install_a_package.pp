527b33ce57cfcf00c5f51b2f2fc56934299dd495
“PoisebTechHub” <sadikadams23@gmail.com>
2024-04-23 19:53:38 +0000

    successful

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,14 +3,12 @@ package { 'Python3': # package to be installed
   ensure   => '3.8.10', # installation of the latest version of package
   provider   => 'pip3', # provider to install package
 }
+package { 'flask': # package to be installed
+  ensure   => '2.1.0', # installation of the lastest version of package
+  provider   => 'pip3', # provider to install package
+}
 #
 package { 'Werkzeug': # package to be installed
   ensure   => '2.1.1', # installation of the latest version of package
   provider   => 'pip3', # provider to install package
 }
-#
-package { 'flask': # package to be installed
-  ensure   => '2.1.0', # installation of the lastest version of package
-  provider   => 'pip3', # provider to install package
-}
-
