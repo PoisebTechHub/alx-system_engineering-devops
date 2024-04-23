9c40174197ea4e0fa4235e037694652161c3f60c
“PoisebTechHub” <sadikadams23@gmail.com>
2024-04-23 18:13:01 +0000

    successful

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,3 @@
-#!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
 package { 'Werkzeug': # package to be installed
   ensure   => '2.1.1', # installation of the latest version of package
