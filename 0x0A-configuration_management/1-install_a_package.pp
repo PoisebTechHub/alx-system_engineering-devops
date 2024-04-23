caa2de41a1ce74560c048702ce6de8d562e2ea66
“PoisebTechHub” <sadikadams23@gmail.com>
2024-04-23 18:45:17 +0000

    successful

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,10 @@
 # Install an especific version of flask (2.1.0)
-package {'Werkzeug': # package to be installed
+package { 'Werkzeug': # package to be installed
   ensure   => '2.1.1', # installation of the latest version of package
   provider   => 'pip3', # provider to install package
 }
-package {'flask': # package to be installed
+#
+package { 'flask': # package to be installed
   ensure   => '2.1.0', # installation of the lastest version of package
   provider   => 'pip3', # provider to install package
 }
