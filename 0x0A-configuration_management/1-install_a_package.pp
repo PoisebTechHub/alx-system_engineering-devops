35316177a130aafc8df1fc8d6c2cd8c588065b9b
“PoisebTechHub” <sadikadams23@gmail.com>
2024-04-27 14:09:54 +0000

    incomplete

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
-# Install an especific version of flask (2.1.0)
-package { 'Python3': # package to be installed
-  ensure   => '3.8.10', # installation of the latest version of package
-  provider   => 'pip3', # provider to install package
+# installs the package puppet-lint
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-package { 'flask': # package to be installed
-  ensure   => '2.1.0', # installation of the lastest version of package
-  provider   => 'pip3', # provider to install package
-}
-#
-package { 'Werkzeug': # package to be installed
-  ensure   => '2.1.1', # installation of the latest version of package
-  provider   => 'pip3', # provider to install package
-}
-
