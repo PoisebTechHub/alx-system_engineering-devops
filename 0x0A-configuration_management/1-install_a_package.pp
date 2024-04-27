52d06ee3af5b1eb0242aad1a561f4d41fd789eea
“PoisebTechHub” <sadikadams23@gmail.com>
2024-04-27 14:13:35 +0000

    incomplete

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
 # installs the package puppet-lint
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+#
+package { 'Werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
